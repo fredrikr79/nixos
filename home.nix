c8a4d3ca7de9fbc9b0f34648a303a7e217d8ba08
fredrikr79 <fredrikrobertsen7@gmail.com>
2025-10-19 18:08:22 +0200

    add qutebrowser

--- home.nix
+++ home.nix
@@ -1348,4 +1348,35 @@ in
     iconTheme.package = pkgs.dracula-icon-theme;
     iconTheme.name = "Dracula";
   };
+
+  programs.qutebrowser = {
+    enable = true;
+    settings = {
+      colors = {
+        hints = {
+          bg = "#000000";
+          fg = "#ffffff";
+        };
+        tabs.bar.bg = "#000000";
+      };
+      # tabs.tabs_are_windows = true;
+      url.start_pages = [ "https://kagi.com/" ];
+    };
+    searchEngines = {
+      w = "https://en.wikipedia.org/wiki/Special:Search?search={}&go=Go&ns0=1";
+      aw = "https://wiki.archlinux.org/?search={}";
+      nw = "https://wiki.nixos.org/index.php?search={}";
+      g = "https://www.google.com/search?hl=en&q={}";
+      DEFAULT = "https://kagi.com/search?q={}";
+    };
+    keyBindings = {
+      normal = {
+        ",v" = "spawn mpv {url}";
+      };
+    };
+    perDomainSettings = {
+      "accounts.google.com".content.headers.user_agent =
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36";
+    };
+  };
 }
